1a5a612e9946f0ebd713d86eadb820f07a889c89
Guanshen77 <a1598875787@gmail.com>
2023-12-11 23:06:50 +0800

    “12/11_audio_concat”

--- src/PD-FGC-inference/inference.py
+++ src/PD-FGC-inference/inference.py
@@ -18,6 +18,10 @@ mp4_dir = "test/mp4s/"
 os.makedirs(save_dir, exist_ok=True)
 os.makedirs(mp4_dir, exist_ok=True)
 
+def mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
 if __name__ == "__main__":
 
     tester = Tester(cfg)
@@ -31,11 +35,40 @@ if __name__ == "__main__":
     eye_path = cfg.eye_path
     audio_folder_path = cfg.audio_folder_path
     exp_folder_path = cfg.exp_folder_path
+    mkdir("test/mp4s/{}/".format(cfg.name))
+    
+    # 列出資料夾內的所有內容
+    for filename in os.listdir("test/mp4s/{}/".format(cfg.name)):
+        file_path = os.path.join("test/mp4s/{}/".format(cfg.name), filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                # 如果是檔案或符號連結，直接刪除
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                # 如果是子目錄，使用 shutil.rmtree() 刪除
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f"刪除 {file_path} 時發生錯誤: {e}")
     
     with open(json_path) as file:
         file_contents = json.load(file)
         print(file_contents["emotion_prediction"])
         exp_predictions = file_contents["emotion_prediction"]
+
+    _save_dir = "test/images/{}".format(cfg.name)
+    os.makedirs(_save_dir, exist_ok=True)
+    # 列出資料夾內的所有內容
+    for filename in os.listdir(_save_dir):
+        file_path = os.path.join(_save_dir, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                # 如果是檔案或符號連結，直接刪除
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                # 如果是子目錄，使用 shutil.rmtree() 刪除
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f"刪除 {file_path} 時發生錯誤: {e}")
 
     frame_list = []
     mp4_list = []
@@ -47,9 +80,6 @@ if __name__ == "__main__":
         exp_path = exp_folder_path + exp_predictions[i]['expression']
         driving_path = [pose_path, exp_path, eye_path]
 
-        _save_dir = "test/images/{}".format(cfg.name)
-        os.makedirs(_save_dir, exist_ok=True)
-        
         frame_dict = {}
         images_return = tester.inference(audio_path, app_img_path, driving_path)
         #得知上一個影片的最後一個frame
@@ -64,16 +94,17 @@ if __name__ == "__main__":
             frame_dict['idx'] = idx + last_frame
 
         frame_list.append(frame_dict)
-        save_mp4 = "test/mp4s/{}.mp4".format(cfg.name + '_' +str(i))
-        mp4_list.append(save_mp4.replace("test/mp4s/",''))
+        save_mp4 = "test/mp4s/{}/{}.mp4".format(cfg.name, cfg.name + '_' +str(i))
+        mp4_list.append(save_mp4.replace("test/mp4s/{}/".format(cfg.name),''))
         command = "ffmpeg -y -start_number {} -r 25 -i {}/%04d.jpg -i {} {}".format(last_frame, _save_dir, audio_path, save_mp4)
         os.system(command)
         
-    txt_path = "test/mp4s/{}.txt".format(cfg.name)
+    txt_path = "test/mp4s/{}/{}.txt".format(cfg.name, cfg.name)
     with open(txt_path, 'w') as file:
         for i in range(len(mp4_list)):
             file.write('file \'{}\'\n'.format(mp4_list[i]))
 
-    concat_mp4 = "test/mp4s/{}.mp4".format(cfg.name + '_' + 'concat')
+    concat_mp4 = "test/mp4s/{}/{}.mp4".format(cfg.name, cfg.name + '_' + 'concat')
     command = "ffmpeg -f concat -i {} -c copy {}".format(txt_path, concat_mp4)
     os.system(command)
+    print("final output in {}".format(concat_mp4))
--- src/PD-FGC-inference/scripts/inference.sh
+++ src/PD-FGC-inference/scripts/inference.sh
@@ -1,11 +1,11 @@
 
 ### case 5
-CUDA_VISIBLE_DEVICES=0 python inference.py --all --name test5 \
-    --audio_folder_path "/home/kevinchiu/Documents/CVPDL/Multi-Emotion-Video-Generation/src/Audio_to_Emotion/audio_split/audio_sample/" \
-    --exp_folder_path "data/motions/exp/" \
+CUDA_VISIBLE_DEVICES=0 python inference.py --all --name test1 \
+    --audio_folder_path "../Audio_to_Emotion/audio_split/audio_sample/" \
+    --exp_folder_path "../Audio_to_Emotion/emotion_image/" \
     --app_img_path "data/apps/tsai.jpg" \
     --pose_path "data/motions/pose/demo_trump" \
     --eye_path "data/motions/eye/reagan_clip1_cropped" \
-    --json_path /home/kevinchiu/Documents/CVPDL/Multi-Emotion-Video-Generation/src/Audio_to_Emotion/emotion_prediction/audio_sample.json
+    --json_path "../Audio_to_Emotion/emotion_prediction/audio_sample.json"
 
 
